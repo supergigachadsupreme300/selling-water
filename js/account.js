a57bdc0378320972e459a2772f66fae413ef79a6
Eser1786 <thaianu2006@gmail.com>
2025-10-30 23:34:42 +0700

    đăng ký/ đăng nhập type shii

--- js/account.js
+++ js/account.js
@@ -1,15 +1,70 @@
+function switchToLogin() {
+  document.getElementById("loginForm").style.display = "block";
+  document.getElementById("registerForm").style.display = "none";
+}
+
+function switchToRegister() {
+  document.getElementById("loginForm").style.display = "none";
+  document.getElementById("registerForm").style.display = "block";
+}
+
+function switchToAccount() {
+  document.getElementById("loginForm").style.display = "none";
+  document.getElementById("registerForm").style.display = "none";
+  document.getElementById("accountForm").style.display = "flex";
+}
+
+function registerToLogin(event) {
+  event.preventDefault();
+
+  const userName = document.getElementById("registerInput").value;
+
+  if (!userName) {
+    alert("vui lòng nhập tên đăng nhập!");
+    return false;
+  }
+
+  const password = document.getElementById("password").value;
+  const repeatedPassword = document.getElementById("repeatedPassword").value;
+
+  if (password !== repeatedPassword) {
+    alert("vui lòng nhập đúng mật khẩu!");
+    return false;
+  }
+
+  alert("Đăng ký thành công");
+
+  localStorage.setItem("userName", userName);
+  localStorage.setItem("password", password);
+
+  switchToLogin();
+}
+
+function Login() {
+  const loginUserName = document.getElementById("loginUserName").value;
+  const loginPassword = document.getElementById("loginPassword").value;
+
+  const userName = localStorage.getItem("userName");
+  const password = localStorage.getItem("password");
+
+  if (userName === loginUserName && loginPassword === password) {
+    alert("Đăng nhập thành công!");
+    switchToAccount();
+  } else {
+    alert("Tên đăng nhập hoặc mật khẩu không đúng!");
+  }
+}
+
 function luuDuLieu() {
   // tạo biến data để lưu dữ liệu người dùng
   const data = {
     name: document.getElementById("customer-name").value, // lưu theo gọi value của id
     phone: document.getElementById("customer-phone").value,
     DOB: document.getElementById("customer-DOB").value,
-    userName: document.getElementById("customer-user-name").value,
+    userName: localStorage.getItem("userName"),
     email: document.getElementById("customer-email").value,
     address: document.getElementById("customer-address").value,
   };
-  // Xuất ra màn hình đã lưu dữ liệu thành công
-  console.log("Dữ liệu của bạn đã được lưu !");
 
   // lưu vào localStorage: kho lưu trữ cục bộ
   localStorage.setItem("thongTinKhachHang", JSON.stringify(data));
